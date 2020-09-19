514a29d185c4fd7dc38ab39a15abd49680104d6f
unknown <14sudhansh@gmail.com>
2020-09-19 17:19:40 +0530

    filter screen

--- Screens/MealDetailScreen.js
+++ Screens/MealDetailScreen.js
@@ -1,28 +1,68 @@
 import React, { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, ImageBackground, FlatList, Image } from 'react-native';
 import { MEALS } from '../data/dummy-data';
 
 const MealDetailScreen = prop => {
     const mealId = prop.route.params.mealId;
     const catId = prop.route.params.categoryId;
     const selectedMeal = MEALS.find(meal => meal.id === mealId);
-    
     useEffect(() => {
-        prop.navigation.setOptions({ title: selectedMeal.title + ' Details' });
+        prop.navigation.setOptions({ title: selectedMeal.title });
     });
 
     return (
-        <View>
-            <Text>{selectedMeal.title}</Text>
-        </View>
+        <ScrollView>
+            <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+            <View style={styles.details}>
+                <Text style={styles.textPadding}>{selectedMeal.duration}m</Text>
+                <Text style={styles.textPadding}>{selectedMeal.complexity.toUpperCase()}</Text>
+                <Text style={styles.textPadding}>{selectedMeal.affordability.toUpperCase()}</Text>
+            </View>
+            <View>
+                <Text style={styles.title}>Ingrdients</Text>
+                <View>
+                    {
+                        selectedMeal.ingrdients.map((item, index) => (
+                            <Text style={styles.listItem} key={index}> {item} </Text>
+                        ))
+                    }
+                </View>
+            </View>
+            <View>
+                <Text style={styles.title}>Steps</Text>
+                <View>
+                    {
+                        selectedMeal.steps.map((item, index) => (
+                            <Text style={styles.listItem} key={index}>
+                                {item}
+                            </Text>))
+                    }
+                </View>
+            </View>
+        </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+    image: {
+        width: '100%',
+        height: 200
+    },
+    details: {
+        flexDirection: 'row',
+        padding: 15,
+        justifyContent: 'space-around'
+    },
+    title: {
+        fontSize: 22,
+        textAlign: 'center'
+    },
+    listItem: {
+        marginVertical: 10,
+        marginHorizontal: 20,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 10
     }
 });
 
